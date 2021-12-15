a330ff59cde7db7a0f7aba552a00b66ad7e933ab
adminwaterlab <waterlab2.server@gmail.com>
2021-12-15 21:59:10 +0800

    update some html page

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,5 @@
 $("#fullname").html(localStorage.getItem("fname") + ' ' + localStorage.getItem("lname"));
+if ("serviceWorker" in navigator) {
+  // register service worker
+  navigator.serviceWorker.register("service-worker.js");
+}
