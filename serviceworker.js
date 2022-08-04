a5e833750ade6e2de4b91b14fa963bbe836da917
sunairphil <sunaircargophil@gmail.com>
2022-08-04 11:30:36 +0800

    update html pages

---
+++ serviceworker.js
@@ -0,0 +1,24 @@
+const staticDevCoffee = "wlapps v1"
+const assets = [
+  "/",
+  "/index.html",
+  "/css/signin.css",
+  "/css/custom.css",
+  "/js/app.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
