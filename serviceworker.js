2d706f576e3e485d8966bd46f4e0f9a709340ea9
sunairphil <sunaircargophil@gmail.com>
2022-08-08 10:28:09 +0800

    update html pages

--- serviceworker.js
+++ serviceworker.js
@@ -1,24 +1,20 @@
-const staticDevCoffee = "wlapps v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/css/signin.css",
-  "/css/custom.css",
-  "/js/app.js"
-]
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+var cacheName = 'hello-pwa'; 
+var filesToCache = [
+'/',    
+'/index.html',    
+'/css/custom.css',  
+'/js/main.js'  ];  
+self.addEventListener('install', function(e) { 
+e.waitUntil(
+caches.open(cacheName).then(function(cache) { 
+return cache.addAll(filesToCache);   
+})    
+);  
+}); 
+/* Serve cached content when offline */ 
+self.addEventListener('fetch', function(e) {  
+e.respondWith(      caches.match(e.request).then(function(response) {  
+return response || fetch(e.request);
+})   
+);  
+});
