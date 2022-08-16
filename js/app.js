a7bd0d22eccadb7605feecc251b685055dd4d37c
sunairphil <sunaircargophil@gmail.com>
2022-08-16 13:26:18 +0800

    edit manifest files

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  navigator.serviceWorker.register('./service-worker.js');
+    console.log('Service Worker is registered');
 }
--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,8 @@
 var cacheName = 'hello-pwa'; 
 var filesToCache = [
 '/',    
-'/index.html',    
+'/index.html', 
+'/welcome.html',    
 '/css/custom.css',  
 '/js/main.js'  ];  
 self.addEventListener('install', function(e) { 
@@ -12,9 +13,21 @@ return cache.addAll(filesToCache);
 );  
 }); 
 /* Serve cached content when offline */ 
-self.addEventListener('fetch', function(e) {  
-e.respondWith(      caches.match(e.request).then(function(response) {  
-return response || fetch(e.request);
-})   
-);  
+
+/*self.addEventListener('install', function(event){
+  console.log('[Service Worker] Installing Service Worker.....',event);
+});*/
+
+self.addEventListener('activate', function(event){
+  console.log('[Service Worker] Activating Service Worker.....',event);
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  console.log('[Service Worker] Fetch initiated.....',event);
+  event.respondWith(fetch(event.request));
+});
+
+self.addEventListener('beforeinstallprompt', (e) => {
+    deferredPrompt = e;
 });
