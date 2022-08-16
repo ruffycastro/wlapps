d369c72b9f0812fbe1d6474a404afcd488d928e1
sunairphil <sunaircargophil@gmail.com>
2022-08-16 13:56:19 +0800

    edit manifest files

--- serviceworker.js
+++ serviceworker.js
@@ -1,22 +1,6 @@
-var cacheName = 'hello-pwa'; 
-var filesToCache = [
-'/',    
-'/index.html', 
-'/welcome.html',    
-'/css/custom.css',  
-'/js/main.js'  ];  
-self.addEventListener('install', function(e) { 
-e.waitUntil(
-caches.open(cacheName).then(function(cache) { 
-return cache.addAll(filesToCache);   
-})    
-);  
-}); 
-/* Serve cached content when offline */ 
-
-/*self.addEventListener('install', function(event){
+self.addEventListener('install', function(event){
   console.log('[Service Worker] Installing Service Worker.....',event);
-});*/
+});
 
 self.addEventListener('activate', function(event){
   console.log('[Service Worker] Activating Service Worker.....',event);
