2e1becaf316049465d8b092d5fb7429253311d60
sunairphil <sunaircargophil@gmail.com>
2022-08-18 13:04:46 +0800

    edit page

--- serviceworker.js
+++ serviceworker.js
@@ -29,7 +29,7 @@ self.addEventListener('fetch', function(event) {
             .catch(function(err) {       // fallback mechanism
               return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
                 .then(function(cache) {
-                  return cache.match('/offline.html');
+                  return cache.match('offline.html');
                 });
             });
         }
