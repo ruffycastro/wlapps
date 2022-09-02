9c943bbc3e94cda76edcf91183c093d3f8c532ac
sunairphil <sunaircargophil@gmail.com>
2022-09-02 16:57:18 +0800

    edit service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,6 +1,29 @@
+const CACHE_NAME = 'wl-apps-v1';
+
+const INITIAL_CACHED_RESOURCES = [
+    '/',
+    '/offline/',
+    '/all/',
+    '/browser/edge/',
+    '/browser/safari/',
+    '/browser/firefox/',
+    '/browser/chrome/',
+    '/css/custome.css',
+    '/images/icons/',
+    '/js/'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(INITIAL_CACHED_RESOURCES);
+    })());
+});
+/*
 self.addEventListener('install', function(event){
   console.log('[Service Worker] Installing Service Worker.....',event);
 });
+*/
 
 self.addEventListener('activate', function(event){
   console.log('[Service Worker] Activating Service Worker.....',event);
