bb2358441d231408f81b7ed5610a80c6a6a096af
sunairphil <sunaircargophil@gmail.com>
2022-10-14 13:14:37 +0800

    edit addtest

--- js/custom.js
+++ js/custom.js
@@ -3,7 +3,9 @@ self.addEventListener("load", function(){
   window.location = 'login.html';
 }
   });
-$("#fullname").html(localStorage.getItem("fname") + ' ' + localStorage.getItem("lname"));
+$("#footer_fullname").html("<p class=\"text-muted\" id=\"fullname\"></p><span class=\"text-right\">Signed in as</span>");
+$("#fullname").html(localStorage.getItem("lsacode"));
+//$("#fullname").html(localStorage.getItem("fname") + ' ' + localStorage.getItem("lname"));
 $("#fname").html("Hi, " + localStorage.getItem("fname"));
 if ("serviceWorker" in navigator) {
   // register service worker
