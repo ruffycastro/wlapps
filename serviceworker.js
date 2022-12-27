cf466c6aa796e99da48ba7fb4147b668a1ff6313
adminwaterlab <96127976+adminwaterlab@users.noreply.github.com>
2022-12-27 08:51:04 +0800

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -29,10 +29,10 @@ self.addEventListener('activate', function(event){
   console.log('[Service Worker] Activating Service Worker.....',event);
   return self.clients.claim();
 });
-self.addEventListener('fetch', function(event){
+/*self.addEventListener('fetch', function(event){
   console.log('[Service Worker] Fetch initiated.....',event);
   event.respondWith(fetch(event.request));
-});       
+}); */      
 
 self.addEventListener('beforeinstallprompt', (e) => {
     deferredPrompt = e;
